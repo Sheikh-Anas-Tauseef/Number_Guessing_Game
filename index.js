234e97dbf44572f802ac41b7a016e782d80a382a
Muhammad Anas <sheikhanastauseef@gmail.com>
2024-04-03 16:37:01 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+#! usr/bin/env node 
+import inquirer from "inquirer";
+console.log("Welcome to CodewithHamza CLI Number Guessing Game");
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Enter your guess number (Number Limit from 1 to 5):",
+    }
+]);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("Congratulations! You guessed the correct number.");
+}
+else {
+    console.log("Sorry, you guessed the wrong number.");
+}
